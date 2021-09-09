a3c1a0b8eb0e9f65459417c9da9fd7f0487e479c
Marcelo <marcelo@Marcelos-MacBook-Pro.local>
2021-09-09 14:40:32 -0300

    fix: fix queues

--- apps/ae_backend/src/controllers/home.ts
+++ apps/ae_backend/src/controllers/home.ts
@@ -6,22 +6,26 @@ import BaseController from './base';
 
 @Service()
 class HomeController extends BaseController {
-    public path = '/';    
+    public path = '/';   
+    queue: Bull.Queue; 
 
     constructor() {
         super();
+        this.queue = new Bull('intensive_queue', 'redis://127.0.0.1:6379');
         this.initializeRouter();
     }
 
-    home(req: express.Request, res: express.Response, next: express.NextFunction): void {
+    home = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
         res.json({ message: 'Welcome to AgileEngine!' });
         next();
     }
 
-    long(req: express.Request, res: express.Response, next: express.NextFunction): void {
-        const queue: Bull.Queue = new Bull('intensive_queue', 'redis://127.0.0.1:6379');
-        const job = queue.add({
-            foo: 'bar'
+    long = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+        const job = this.queue.add({
+            foo: 'bar',
+        }, {
+            attempts: 5,
+            backoff: 5000
         });
 
         job.then(data => {
@@ -33,7 +37,7 @@ class HomeController extends BaseController {
         res.sendStatus(200);
     }
 
-    protected initializeRouter(): void {
+    protected initializeRouter = (): void => {
         this.router.get(this.path, this.home);
         this.router.post(this.path, this.long);
     }
